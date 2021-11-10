60fdd5dba59ada0e6992bdaaf5f9f6d827c4632a
UH14 <94074227+UH14@users.noreply.github.com>
2021-11-11 01:14:41 +0530

    Add files via upload

---
+++ ov/ov/len of 1st two vectors in given list.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 11:01:53 2021
+
+@author: Karthikeyan
+"""
+
+'''import numpy as np
+list1=[np.array([1,2,3,4]), 'Program', 1.234]
+list2=[range(1,5), "R", 12.0]
+print(type(list1[0]))
+print(list2)
+print(type(list2[0]))'''
+
+
+'''list_data = ["Python", "PHP", [5,7,9,11], True, 125.17, 75.83]
+print("Data of the list: ")
+print(list_data)'''
+
+n1=[1,2,3]
+c1 = ["Red", "Green", "Black"]
+print("Original lists: ")
+print(n1)
+print(c1)
+print("Merge the said lists: ")
+mlist = n1 + c1
+print("New merged list: ")
+print(mlist)
